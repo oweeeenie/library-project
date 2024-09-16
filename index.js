4b2148926ab5d594a9689943ac2bdab860a1f51f
oweeeenie <owensullivan419@yahoo.com>
2024-09-15 21:47:23 -0400

    basic JS and HTML created

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const myLibrary = [];
 
-function Book() {
-  
+function Book(title, author, pages, readStatus) {
+this.title = title;
+this.author = author;
+this.pages = pages;
+this.readStatus = readStatus;
 }
 
 function addBookToLibrary() {
-
+const title = document.querySelector("#title").value;
+const author = document.querySelector("#author").value;
+const pages = document.querySelector("#pages").value;
+const readStatus = document.querySelector("#readStatus").value;
+const newBook = new Book(title, author, pages, readStatus);
+myLibrary.push(newBook);
 }
+
+const bookForm = document.querySelector("#bookForm");
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+});
+
